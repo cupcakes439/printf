cdc6a47c8436760a58bcc9d5682bf93fde53b6eb
cupcakes439 <adeolaakande439@gmail.com>
2022-10-19 07:52:41 -0700

    Tasks

---
+++ functions1.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ * found_reverse - reverses a string
+ * @s: string to reverse
+ * Return: pointer to string
+ */
+char *found_reverse(va_list s)
+{
+	char *rstring, *hold;
+	int i, j, len;
+
+	hold = va_arg(s, char *);
+	len = _strlen(hold);
+	rstring = malloc((len + 1) * sizeof(char));
+	for (i = 0, j = len - 1; i < len; i++, j--)
+		rstring[i] = hold[j];
+
+	return (rstring);
+}
+
+/**
+ * found_rot13 - rotate 13 characters per character in the string
+ * @s: string to rot13
+ * Return: string after conversion
+ */
+char *found_rot13(va_list s)
+{
+	int i, size;
+	char *arg;
+	char *hold;
+	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char storel[] = "nopqrstuvwxyzabcdefghijklm";
+
+	arg = va_arg(s, char *);
+	size = _strlen(arg);
+	hold = malloc((size + 1) * sizeof(char));
+	for (i = 0; arg[i] != '\0'; i++)
+	{
+		if ((arg[i] >= 'A' && arg[i] <= 'Z') ||
+		    (arg[i] >= 'a' && arg[i] <= 'z'))
+		{
+			hold[i] = (arg[i] - 'A' > 25) ?
+				storel[arg[i] - 'a'] : storeh[arg[i] - 'A'];
+		}
+		else
+			hold[i] = arg[i];
+	}
+	hold[i] = '\0';
+	return (hold);
+}
---
+++ functions2.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+  * found_octal - converts decimal numbers to octal
+  * @n: octal number to print
+  * Return: pointer to string
+  */
+char *found_octal(va_list n)
+{
+	unsigned int pos, temp, result, oct, i;
+	char *string;
+
+	oct = va_arg(n, unsigned int);
+	result = i = 0;
+	pos = 1;
+	while (oct)
+	{
+		result += (oct % 8) * pos;
+		oct /= 8;
+		pos *= 10;
+		i++;
+	}
+	temp = i;
+	pos /= 10;
+	string = malloc(temp * sizeof(char) + 1);
+	for (i = 0 ; i < temp; i++)
+	{
+		string[i] = ((result / pos) % 10) + '0';
+		pos /= 10;
+	}
+	string[i] = '\0';
+	return (string);
+}
